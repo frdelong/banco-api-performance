f36b9e92653e89d3a71069da3da4b68ef5cc8859
frdelong <frdelong@gmail.com>
2025-08-20 02:41:55 -0300

    Refactor test options to use stages for load testing configuration

--- tests/login.test.js
+++ tests/login.test.js
@@ -7,8 +7,16 @@ import { sleep, check } from 'k6'
 export const options = {
   // Define the number of iterations for the test
   // iterations: 30,
-  vus: 10,
-  duration: '30s',
+  // vus: 10,
+  // duration: '30s',
+  stages: [
+    { duration: '10s', target: 10 },
+    { duration: '20s', target: 10 },
+    { duration: '10s', target: 30 },
+    { duration: '20s', target: 30 },
+    { duration: '20s', target: 0 }
+  ],
+
   thresholds: {
     http_req_duration: ['p(90)<3000', 'max<5000'],
     http_req_failed: ['rate<0.01']
